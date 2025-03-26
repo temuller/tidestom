2fdad8127ed84a89cefccf2f0ba489f60978ea08
phil <philip@wisemen.co.uk>
2025-03-26 11:45:36 +0000

    remove unwanted files from tracking"

--- tidestom/.ipynb_checkpoints/views-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-from tom_observations.models import BaseTarget as Target
-
-
-class AboutView(TemplateView):
-    template_name = 'about.html'
-
-    def get_context_data(self, **kwargs):
-        return {'targets': Target.objects.all()}
