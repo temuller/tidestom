6cf3099f4a5499a4117ec05a52f8733bb1f2a906
Chris Duffy <c.jduffy@me.com>
2025-06-12 14:32:45 +0100

    Custom Aladin Code
    Created and connected custom Aladin widget with marker the size of a fibre

--- custom_code/templatetags/tides_targets_extras.py
+++ custom_code/templatetags/tides_targets_extras.py
@@ -1,6 +1,5 @@
 from django import template
 from django.conf import settings
-from tom_targets.models import Target, TargetExtra
 register = template.Library()
 
 @register.inclusion_tag('custom_code/partials/target_data.html')
@@ -10,7 +9,7 @@ def tides_target_data(target):
     """
     exclude_fields = ['name', 'tidesclass', 'tidesclass_other', 'tidesclass_subclass', 'auto_tidesclass', 'auto_tidesclass_other', 'auto_tidesclass_subclass', 'auto_tidesclass_prob', 'human_tidesclass', 'human_tidesclass_other', 'human_tidesclass_subclass']
     extras = {k['name']: target.extra_fields.get(k['name'], '') for k in settings.EXTRA_FIELDS if not k.get('hidden') and k['name'] not in exclude_fields}
-    print(target.as_dict())  
+    print(target.as_dict())
     return {
         'target': target,
         'extras': extras
@@ -19,3 +18,13 @@ def tides_target_data(target):
 @register.inclusion_tag('custom_code/partials/target_classifications.html')
 def target_classifications(target):
     return {'target': target}
+
+@register.inclusion_tag('custom_code/partials/aladin_finderchart.html')
+def aladin_finderchart(target):
+    """
+    Displays Aladin skyview of the given target along with basic finder chart annotations including a compass
+    and a scale bar. The resulting image is downloadable. This templatetag only works for sidereal targets.
+    """
+
+    return {'target': target}
+
